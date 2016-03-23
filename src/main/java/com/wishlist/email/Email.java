ba827e4ddfdd804f8a63874402533a6ee406f222
Pranav Sundriyal <psundriyal@expedia.com>
2016-03-23 18:13:06 -0500

    improve message

--- src/main/java/com/wishlist/email/Email.java
+++ src/main/java/com/wishlist/email/Email.java
@@ -7,10 +7,6 @@ import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 
-
-/**
- * Created by psundriyal on 3/23/16.
- */
 public class Email {
 
     public void sendMail(SlimResponse response, Rule rule){
@@ -18,12 +14,10 @@ public class Email {
         final String username = "wishlistexpedia@gmail.com";
         final String password = "";
         final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-        // Get a Properties object
         Properties props = System.getProperties();
         props.setProperty("mail.smtp.host", "smtp.gmail.com");
         props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
         props.setProperty("mail.smtp.socketFactory.fallback", "false");
-       // props.setProperty("mail.smtp.port", "465");
         props.setProperty("mail.smtp.socketFactory.port", "465");
         props.put("mail.smtp.auth", "true");
         props.put("mail.debug", "true");
@@ -36,15 +30,13 @@ public class Email {
                         protected PasswordAuthentication getPasswordAuthentication() {
                             return new PasswordAuthentication(username, password);
                         }});
-            // -- Create a new message --
             Message msg = new MimeMessage(session);
 
-            // -- Set the FROM and TO fields --
-            msg.setFrom(new InternetAddress("wishlist@gmail.com"));
+            msg.setFrom(new InternetAddress(username));
             msg.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(rule.getEmail(),false));
-            msg.setSubject("wishlist");
-            msg.setText("found cheap tickets");
+            msg.setSubject(new WishListMessage().createSubject(rule));
+            msg.setText(new WishListMessage().createMessage(rule, response));
             msg.setSentDate(new Date());
             Transport.send(msg);
         }catch (MessagingException e){ System.out.println("error cause: " + e);}
---
+++ src/main/java/com/wishlist/email/WishListMessage.java
@@ -0,0 +1,45 @@
+package com.wishlist.email;
+
+import com.wishlist.model.Leg;
+import com.wishlist.model.rule.Criteria;
+import com.wishlist.model.rule.Rule;
+import com.wishlist.model.slim.SearchResult;
+import com.wishlist.model.slim.SlimResponse;
+
+/**
+ * Created by psundriyal on 3/23/16.
+ */
+public class WishListMessage {
+
+    public String createMessage(Rule rule, SlimResponse response){
+
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("\nFor Trip\n\n");
+
+        createSubject(rule);
+        sb.append("\n\n");
+
+        for (Criteria criteria : rule.getCriterias()){
+            sb.append(criteria.getCriteriaType()).append("-").append(criteria.getCriteriaMap().toString()).append("\n");
+        }
+
+        sb.append("\nPrices found for the above Criteria\n");
+
+        for (SearchResult searchResult : response.getSearchResultList()){
+            sb.append(searchResult.getPrice()).append("\n");
+        }
+
+
+        return sb.toString();
+    }
+
+    public String createSubject(Rule rule){
+        StringBuffer sb = new StringBuffer();
+        sb.append(rule.getOrigin()).append("-").append(rule.getDestination()).append("/")
+                .append(rule.getDeparturteDate()).append("/").append(rule.getArrivalDate()).append("\n");
+
+        return sb.toString();
+    }
+
+}
